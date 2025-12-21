664ba23b755b43252dee071d8517649a4af39600
Gloria R <norimaqidoe@hotmail.com>
2025-12-21 00:30:24 -0600

    feat: create Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import logo from "./assets/images/logo.svg";
-import profile from "./assets/images/Cindy-Campbell.jpeg";
+import Main from "./components/main/Main";
 
 function App() {
   return (
@@ -7,21 +7,7 @@ function App() {
       <header class="header page__section">
         <img class="header__image" src={logo} alt="Around the U.S.logo" />
       </header>
-
-      <main class="content">
-        <section class="profile page__section">
-          <div class="profile__image-container">
-            <img class="profile__image" src={profile} alt="Imágen de perfil" />
-            <div class="profile__image-edit"></div>
-          </div>
-          <div class="profile__info">
-            <h1 class="profile__title">Cindy Campbell</h1>
-            <button class="profile__edit-button" type="button"></button>
-            <p class="profile__description">Reportera</p>
-          </div>
-          <button class="profile__add-button" type="button"></button>
-        </section>
-      </main>
+      <Main />
 
       <footer class="footer">
         <p class="footer__copyright">&copy; 2025 Gloria Rangel</p>
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,20 @@
+import profile from "./assets/images/Cindy-Campbell.jpeg";
+
+export default function Main() {
+  return (
+    <main class="content">
+      <section class="profile page__section">
+        <div class="profile__image-container">
+          <img class="profile__image" src={profile} alt="Imágen de perfil" />
+          <div class="profile__image-edit"></div>
+        </div>
+        <div class="profile__info">
+          <h1 class="profile__title">Cindy Campbell</h1>
+          <button class="profile__edit-button" type="button"></button>
+          <p class="profile__description">Reportera</p>
+        </div>
+        <button class="profile__add-button" type="button"></button>
+      </section>
+    </main>
+  );
+}
