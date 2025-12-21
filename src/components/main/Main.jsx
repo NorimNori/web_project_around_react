a871dac9ed1bed7b73e34d318bbd8a86806c69f6
Gloria R <norimaqidoe@hotmail.com>
2025-12-21 02:19:47 -0600

    feat: add open and close functionality to NewCard popup

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import profile from "../../assets/images/Cindy-Campbell.jpeg";
+import Popup from "../popup/Popup";
+import NewCard from "../newCard/NewCard";
 
 export default function Main() {
+  const [popup, setPopup] = useState(null);
+
+  const newCardPopup = { title: "Nuevo lugar", children: <NewCard /> };
+
+  function handleOpenPopup(popup) {
+    setPopup(popup);
+  }
+
+  function handleClosePopup() {
+    setPopup(null);
+  }
   return (
     <main className="content">
       <section className="profile page__section">
@@ -17,8 +31,18 @@ export default function Main() {
           <button className="profile__edit-button" type="button"></button>
           <p className="profile__description">Reportera</p>
         </div>
-        <button className="profile__add-button" type="button"></button>
+        <button
+          className="profile__add-button"
+          type="button"
+          onClick={() => handleOpenPopup(newCardPopup)}
+        ></button>
       </section>
+
+      {popup && (
+        <Popup onClose={handleClosePopup} title={popup.title}>
+          {popup.children}
+        </Popup>
+      )}
     </main>
   );
 }
--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -1,9 +1,13 @@
 export default function Popup(props) {
-  const { title, children } = props;
+  const { onClose, title, children } = props;
   return (
     <section class="popup" id="profile-edit-popup">
       <div class="popup__container">
-        <button class="popup__close" id="profile-popup-close-button"></button>
+        <button
+          class="popup__close"
+          id="profile-popup-close-button"
+          onClick={onClose}
+        ></button>
         <h3 class="popup__heading">{title}</h3>
         {children}
       </div>
