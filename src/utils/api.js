76d79ddf70d7499f5e95a571f2c3c1db7ad18de4
Gloria R <norimaqidoe@hotmail.com>
2026-01-11 17:02:37 -0600

    feat: add class Api

---
+++ src/utils/api.js
@@ -0,0 +1,52 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  _handleError(error) {
+    console.error(error);
+    return Promise.reject(error);
+  }
+
+  updateUserInfo(name, about) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    })
+      .then(this._checkResponse)
+      .catch(this._handleError);
+  }
+
+  addNewCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    })
+      .then(this._checkResponse)
+      .catch(this._handleError);
+  }
+}
+
+export const api = new Api({
+  baseUrl: "https://around-api.es.tripleten-services.com/v1",
+  headers: {
+    authorization: "abe5a634-69dc-47eb-a847-7bcef8fa75fb",
+    "Content-Type": "application/json",
+  },
+});
