efe5d41b78e1eb90761963ff9b2a7957539ea7d6
Gloria R <norimaqidoe@hotmail.com>
2026-01-11 17:32:20 -0600

    feat: call api in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import Footer from "./components/footer/Footer.jsx";
 import Header from "./components/header/Header.jsx";
 import Main from "./components/main/Main.jsx";
+import { api } from "./utils/api.js";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+  const [cards, setCards] = useState([]);
+
+  function handleUpdateUser({ name, about }) {
+    api
+      .updateUserInfo(name, about)
+      .then((updatedUser) => {
+        setCurrentUser(updatedUser);
+      })
+      .catch(console.error);
+  }
+
+  function handleAddPlaceSubmit(cardData) {
+    api
+      .addNewCard(cardData)
+      .then((newCard) => {
+        setCards([newCard, ...cards]);
+      })
+      .catch(console.error);
+  }
   return (
     <div className="page__section page__content">
       <Header />
-      <Main />
+      <Main
+        currentUser={currentUser}
+        cards={cards}
+        onUpdateUser={handleUpdateUser}
+        onAddPlace={handleAddPlaceSubmit}
+      />
       <Footer />
     </div>
   );
