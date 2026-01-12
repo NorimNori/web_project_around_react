66ddd81ca52a8b7d4a4ca9776504a31d8169364e
Gloria R <norimaqidoe@hotmail.com>
2026-01-11 23:47:53 -0600

    feat: connect profile and card popups to app state and API handlers

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import profile from "../../assets/images/Cindy-Campbell.jpeg";
+import profilePlaceholder from "../../assets/images/Cindy-Campbell.jpeg";
 import Popup from "../popup/Popup";
 import NewCard from "../newCard/NewCard";
 import EditAvatar from "../editAvatar/EditAvatar";
@@ -7,51 +7,13 @@ import EditProfile from "../editProfile/EditProfile";
 import Card from "../card/Card";
 import ImagePreview from "../imagePopup/ImagePreview";
 
-const cards = [
-  {
-    name: "Silent Hill, Toluca Lake",
-    link: "https://plus.unsplash.com/premium_photo-1669689972709-c4a9a9a4cbbe?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "3600 Prospect Street",
-    link: "https://plus.unsplash.com/premium_photo-1676657955502-405b78e1e900?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Wizard Island",
-    link: "https://images.unsplash.com/photo-1706195015965-9ba2e20b5837?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Black Hills Forest",
-    link: "https://plus.unsplash.com/premium_photo-1698501025921-53646a4279c2?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Raccon City, Police station",
-    link: "https://images.unsplash.com/photo-1730492109404-bf4ad0a78f3b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Hotel Timberline",
-    link: "https://images.unsplash.com/photo-1714237570574-bffa98593a68?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
-
-export default function Main() {
+export default function Main({ currentUser, cards, onUpdateUser, onAddPlace }) {
   const [popups, setPopups] = useState({
     newCard: false,
     editProfile: false,
     editAvatar: false,
     imagePreview: null,
   });
-  console.log("STATE imagePreview:", popups.imagePreview);
-
-  const newCardPopup = { title: "Nuevo lugar", children: <NewCard /> };
-  const editAvatarPopup = {
-    title: "Cambiar foto de perfil",
-    children: <EditAvatar />,
-  };
-  const editProfilePopup = {
-    title: "Editar perfil",
-    children: <EditProfile />,
-  };
 
   function handleOpenPopup(name, value = true) {
     setPopups((prev) => ({
@@ -73,29 +35,34 @@ export default function Main() {
         <div className="profile__image-container">
           <img
             className="profile__image"
-            src={profile}
-            alt="Imágen de perfil"
+            src={currentUser.avatar || profilePlaceholder}
+            alt="Imagen de perfil"
           />
           <div
             className="profile__image-edit"
             onClick={() => handleOpenPopup("editAvatar")}
-          ></div>
+          />
         </div>
+
         <div className="profile__info">
-          <h1 className="profile__title">Cindy Campbell</h1>
+          <h1 className="profile__title">
+            {currentUser.name || "Cargando..."}
+          </h1>
           <button
             className="profile__edit-button"
             type="button"
             onClick={() => handleOpenPopup("editProfile")}
           />
-          <p className="profile__description">Reportera</p>
+          <p className="profile__description">{currentUser.about || ""}</p>
         </div>
+
         <button
           className="profile__add-button"
           type="button"
           onClick={() => handleOpenPopup("newCard")}
         />
       </section>
+
       <section className="cards">
         <ul className="cards__list">
           {cards.map((card) => (
@@ -109,33 +76,38 @@ export default function Main() {
       </section>
 
       {popups.newCard && (
-        <Popup
-          onClose={() => handleClosePopup("newCard")}
-          title={newCardPopup.title}
-        >
-          {newCardPopup.children}
+        <Popup title="Nuevo lugar" onClose={() => handleClosePopup("newCard")}>
+          <NewCard
+            onAddPlace={onAddPlace}
+            onClose={() => handleClosePopup("newCard")}
+          />
         </Popup>
       )}
 
       {popups.editProfile && (
         <Popup
-          title={editProfilePopup.title}
+          title="Editar perfil"
           onClose={() => handleClosePopup("editProfile")}
         >
-          {editProfilePopup.children}
+          <EditProfile
+            currentUser={currentUser}
+            onUpdateUser={onUpdateUser}
+            onClose={() => handleClosePopup("editProfile")}
+          />
         </Popup>
       )}
 
       {popups.editAvatar && (
         <Popup
-          title={editAvatarPopup.title}
+          title="Cambiar foto de perfil"
           onClose={() => handleClosePopup("editAvatar")}
         >
-          {editAvatarPopup.children}
+          <EditAvatar onClose={() => handleClosePopup("editAvatar")} />
         </Popup>
       )}
+
       {popups.imagePreview && (
-        <Popup onClose={handleClosePopup} title={null}>
+        <Popup title={null} onClose={() => handleClosePopup("imagePreview")}>
           <ImagePreview
             card={popups.imagePreview}
             onClose={() => handleClosePopup("imagePreview")}
