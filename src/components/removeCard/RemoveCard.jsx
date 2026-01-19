b6c60f00a39892cd554a2ecdbeb0998f61f34e04
Gloria R <norimaqidoe@hotmail.com>
2026-01-18 22:39:15 -0600

    feat: create RemoveCard component

---
+++ src/components/removeCard/RemoveCard.jsx
@@ -0,0 +1,9 @@
+export default function RemoveCard() {
+  return (
+    <form class="popup__form" id="form-confirmation">
+      <button class="popup__save-button" id="popup__confirm-button">
+        Sí
+      </button>
+    </form>
+  );
+}
