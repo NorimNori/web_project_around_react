2af2628ece9c5b1e14e625dd7516c7fb6cb5fc2d
Gloria R <norimaqidoe@hotmail.com>
2026-02-27 20:10:38 -0600

    style: add css for InfoTooltip component

--- src/components/infotooltip/Infotooltip.jsx
+++ src/components/infotooltip/Infotooltip.jsx
@@ -2,21 +2,13 @@ export default function InfoTooltip({ isOpen, onClose, isSuccess }) {
   if (!isOpen) return null;
 
   return (
-    <div className="modal modal_opened">
-      <div className="modal__container modal__container_type_tooltip">
-        <button
-          className="modal__close-button"
-          type="button"
-          onClick={onClose}
-        />
+    <div className="tooltip">
+      <div className="tooltip__container">
+        <button className="tooltip__close" onClick={onClose} />
         <div
-          className={`modal__tooltip-icon ${
-            isSuccess
-              ? "modal__tooltip-icon_type_success"
-              : "modal__tooltip-icon_type_error"
-          }`}
+          className={`tooltip__icon ${isSuccess ? "tooltip__icon_type_success" : "tooltip__icon_type_error"}`}
         />
-        <p className="modal__tooltip-text">
+        <p className="tooltip__text">
           {isSuccess
             ? "¡Correcto! Ya estás registrado."
             : "Uy, algo salió mal. Por favor, inténtalo de nuevo."}
